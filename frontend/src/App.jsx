7a4bb26d2bd8602200af21cd471c82987d4b077a
Tejas Cousik <tcousik0@gmail.com>
2024-01-02 00:46:20 -0500

    second

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
+
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>Yo</div>
+    <div className='App'>
+      <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </div>
   )
   
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <header className="container">
+<Link to="/">
+    <h1>Workout Bud</h1>
+</Link>
+    </header>
+  )
+}
+
+export default Navbar
