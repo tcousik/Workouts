f6834f4c5a9d5eb701c8c4f6d7d918d050c79cc8
Tejas Cousik <tcousik0@gmail.com>
2024-01-02 00:13:09 -0500

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div>Yo</div>
+  )
+  
+}
+
+export default App
