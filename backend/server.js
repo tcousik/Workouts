fdee83748010aab6c4a8ae90b0088aa92df39840
Tejas Cousik <tcousik0@gmail.com>
2024-01-03 13:33:59 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+var cors = require("cors");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
