143a1520ffb5502c300a7a694df06584fb7a4f4e
Tejas Cousik <tcousik0@gmail.com>
2024-01-04 17:03:42 -0500

    delete

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+
+  const handleClick = async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/workouts/" + workout._id,
+      { method: "DELETE" }
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>
@@ -10,6 +25,7 @@ const WorkoutDetails = ({ workout }) => {
       <p>Reps: {workout.reps}</p>
       <p>Sets: {workout.sets}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
