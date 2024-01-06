832c94bf55bcea768d0a291ddc34f5eec2399461
Tejas Cousik <tcousik0@gmail.com>
2024-01-06 16:33:18 -0500

    useLogin hook

---
+++ frontend/src/hooks/useLogin.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("http://localhost:4000/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    } else {
+      // Save user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // Update auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+};
