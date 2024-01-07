315221d7afcfe638c9a15dceba13ca12fb5b5011
Tejas Cousik <tcousik0@gmail.com>
2024-01-07 01:43:14 -0500

    server.js edits

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const path = require("path");
 
 const app = express();
 
@@ -17,9 +18,17 @@ app.use((req, res, next) => {
   next();
 });
 
+// Serve the production build files from the frontend directory
+app.use(express.static(path.join(__dirname, "frontend")));
+
 // Routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
+
+// Serve the main HTML file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/index.html"));
+});
 
 // Connect to DB
 mongoose
